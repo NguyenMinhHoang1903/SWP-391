a79676906a79c2a540a93fbbd9d1cfcbec95a788
chinh <chinh3571113@gmail.com>
2024-07-17 16:28:02 +0700

    Fixed delete users

--- backend/controller/deleteUser.js
+++ backend/controller/deleteUser.js
@@ -4,27 +4,23 @@ async function deleteUser(req, res) {
   try {
     let query = { _id: req.body.userId.toString() };
 
-    await userModel.findById(query)
-    .then(user => {
-      if(user.role === "ADMIN") {
+    await userModel.findById(query).then((user) => {
+      if (user.role === "ADMIN") {
         res.json({
           message: "How dare you delete your boss?",
           success: false,
           error: true,
         });
-      }
-      else {
-        userModel.deleteOne(query);
-        res.json({
-          message: "Deleted User",
-          success: true,
-          error: false,
+      } else {
+        userModel.deleteOne(query).then(() => {
+          res.json({
+            message: "Deleted User",
+            success: true,
+            error: false,
+          });
         });
       }
-    })
-
-    
-    
+    });
   } catch (err) {
     res.status(400).json({
       message: err.message || err,
